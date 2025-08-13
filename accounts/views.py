48f55af33a27a292c926dffc47e7bdf7f92c8b50
Adviye Selin Kaseler <2880864k@student.gla.ac.uk>
2025-08-13 12:42:28 +0100

    UI updates, fixed review and favorites styling, clickable brand

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
+from movies.models import Review, Favorite
 
 def signup_view(request):
     if request.method == 'POST':
@@ -16,7 +17,18 @@ def signup_view(request):
 
 @login_required
 def profile_view(request):
-    return render(request, 'accounts/profile.html', {
-        "profile_user": request.user,
-        "favorites": []
-    })
+    user_reviews = (
+        Review.objects
+        .filter(user=request.user)
+        .select_related("movie")
+        .order_by("-created_at")
+    )
+
+    fav_qs = Favorite.objects.filter(user=request.user).select_related("movie")
+    favorites = [f.movie for f in fav_qs]
+
+    return render(
+        request,
+        "accounts/profile.html",
+        {"favorites": favorites, "user_reviews": user_reviews},
+    )
--- movies/static/movies/js/script.js
+++ movies/static/movies/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll("li a");
     links.forEach(link => {
         link.addEventListener("click", () => {
+            link.style.transition = "transform 0.15s ease";
             link.style.transform = "scale(0.95)";
             setTimeout(() => {
                 link.style.transform = "scale(1)";
